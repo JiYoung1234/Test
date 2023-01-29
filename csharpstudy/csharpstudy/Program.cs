8b517fa4e975b357513437888d430962908a3350
jiyoung1234 <qkrwldud131@naver.com>
2023-01-29 14:04:40 +0900

    hello world

---
+++ csharpstudy/csharpstudy/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharpstudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("hello world");
+        }
+    }
+}
